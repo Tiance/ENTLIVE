de5626b251e16d241e37b78d00fbc336d26c85ef
南风不竞 <Yamazhiki@outlook.com>
2018-03-15 19:23:41 +0800

    添加IM消息回调

---
+++ App/Library/IM/TX/TXIMMessageUtils.swift
@@ -0,0 +1,12 @@
+//
+// Created by Yamazhiki on 15/03/2018.
+// Copyright (c) 2018 ENT-LIVE. All rights reserved.
+//
+
+import Foundation
+
+extension TIMTextElem {
+    func cover() -> IMMessage {
+
+    }
+}
